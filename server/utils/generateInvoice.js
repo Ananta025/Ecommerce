29b3db3abf54b8f40e0869d41e6632f8e1688ae9
Ananta Chandra Das <danantach@gmail.com>
2025-07-18 11:54:14 +0530

    first commit

--- server/utils/generateInvoice.js
+++ server/utils/generateInvoice.js
@@ -91,7 +91,7 @@ const generateInvoice = async (order, type = 'a4') => {
   }
 
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: 'new',
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
